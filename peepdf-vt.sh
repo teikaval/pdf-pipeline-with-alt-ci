564ffba3da353b069afdc57556402660087fe353
saguywalker <saguywalker@protonmail.com>
2019-06-19 13:30:10 +0300

    test pdfid and peepdf using image config in gitlab-ci

--- peepdf-vt.sh
+++ peepdf-vt.sh
@@ -1,41 +1,37 @@
 #!/bin/bash
 SAMPLESPATH=$(pwd)
-#ls $SAMPLESPATH/pdf-source/ -R
-ls $SAMPLESPATH/ -R
+mkdir -p ${SAMPLESPATH}/output-files/results
+ls $SAMPLESPATH/pdf-source/ -R
 
 # Do nothing if folder is empty
-if (( "$(ls $SAMPLESPATH/pdf | wc -l)" == 0  )); then
-#if (( "$(ls $SAMPLESPATH/pdf-source/pdf |wc -l)" == 0 )); then
+if (( "$(ls $SAMPLESPATH/pdf-source/pdf |wc -l)" == 0 )); then
 	echo "Folder is empty"
 else
 	echo "Processing files"
 	cd /peepdf
-	for file in $SAMPLESPATH/pdf/*
-  #for file in $SAMPLESPATH/pdf-source/pdf/*
-    do
-    xbase=${file##*/}; xfext=${xbase##*.}; xpref=${xbase%.*}
-    echo Analysing: ${file##*/}
+	for file in $SAMPLESPATH/pdf-source/pdf/*
+        do
+          xbase=${file##*/}; xfext=${xbase##*.}; xpref=${xbase%.*}
+          echo Analysing: ${file##*/}
 
 	  /usr/bin/python peepdf.py $file -f -c  | sed -r "s/[[:cntrl:]]\[[0-9]{1,3}m//g" > result
-	  #docker run cincan/peepdf $file -f -c | sed -r "s/[[:cntrl:]]\[[0-9]{1,3}m//g" > result
-		cat result | tee -a $SAMPLESPATH/peepdf.log 
+	  cat result | tee -a $SAMPLESPATH/peepdf.log 
 	done
 
 	# Update the results-git
 	cd $SAMPLESPATH
 	ls $SAMPLESPATH -R
-	#git clone results output-files
+	git clone results output-files
 
-	#cd $SAMPLESPATH/output-files
-	#cp $SAMPLESPATH/*.log $SAMPLESPATH/output-files/results/
-  cp $SAMPLESPATH/*.log $SAMPLESPATH/results/
+	cd $SAMPLESPATH/output-files
+	cp $SAMPLESPATH/*.log $SAMPLESPATH/output-files/results/
 
-#	git pull
+	git pull
 	git add .
-#	git config --global user.name "${GITLAB_USER_ID}"
-#	git config --global user.email "${GITLAB_USER_EMAIL}"
-  git status
-  git commit -m "[skip ci] update peepdf results"
+	git config --global user.name "cincan-pipeline"
+	git config --global user.email "cincan@concourse"
+	git commit -m "update peepdf results"
 
 fi
+
 
